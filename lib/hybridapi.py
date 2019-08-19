9104932e4b3cd1032c072202c23a3c94a83fa030
smithac <carter.smith@diamondcyber.com>
2019-08-19 16:04:05 +0930

    added hybridapi

---
+++ lib/hybridapi.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+base = "https://www.hybrid-analysis.com/api/v2" #base URL for API
+key = "XXXXX" #replace with your key
+
+def auth():
+
+
+def searchHash(hash):
+	return 0;
+
+def getStates():
+	return 0;
+
+def searchURL(url):
+	return 0;
+
+def searchFile(file):
+	return 0;
+
+def getReport(id):
+	return 0;
